e71050be281ee4598833d1274433f96c5cd6c741
Mark <markjohnguto@gmail.com>
2025-09-03 20:17:00 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 // Code your solution in this file!
-```js
-describe('index.js', function () {
-  // there's stuff in here, too
-});
-```;
-
- ```js
-it('is set as Scuber',nction () {
-  // tests are here
-});
-```;
-
-```js
-it('is set as Scuber', function () {
-  expect(companyName).to.equal('Scuber');
-});
-```;
-
-
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
 
 
 
